9435c09009cf52d936c95cea6bd283358326e32a
Lunde CHEN <lunde.chen@yahoo.fr>
2020-11-02 19:53:48 +0800

    adaptor adaptee clientcode

---
+++ cmake-build-debug/CMakeFiles/adaptor_example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/adaptor_example.dir/main.cpp.obj"
+  "adaptor_example.exe"
+  "adaptor_example.exe.manifest"
+  "adaptor_example.pdb"
+  "libadaptor_example.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/adaptor_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
